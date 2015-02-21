27ae9985f22326453bb4db7e624116fe395d0b07
Artem Rashev <artem.rashev@gmail.com>
2015-02-22 00:01:26 +0200

    Configured login password system

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,8 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
+import static play.data.Form.*;
 
 import views.html.*;
 
@@ -12,8 +14,30 @@ public class Application extends Controller {
     }
 
     public static Result login() {
-        return ok(login.render()
-    );
-}
+        return ok(login.render());
+    }
+
+
+    public static Result authenticate() {
+    	Form<Login> loginForm = form(Login.class).bindFromRequest();
+	if (loginForm.hasErrors()) {
+        	return badRequest(login.render(loginForm));
+    	} else {
+        	session().clear();
+        	session("email", loginForm.get().email);
+        	return redirect(routes.Application.index());
+    	}    	
+    }
+
+    public static class Login {
+        public String email;
+        public String password;
+	public String validate() {
+    		if (User.authenticate(email, password) == null) {
+      			return "Invalid user or password";
+    		}
+    		return null;
+	}
+    }
 
 }
