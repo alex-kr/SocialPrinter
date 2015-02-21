eb069965330da81c7e66f9d51e8210555e7e45af
dychko <dichko.v@bigmir.net>
2015-02-21 23:35:35 +0200

    Fix Login

--- app/models/Login.java
+++ app/models/Login.java
@@ -1,18 +1,17 @@
 package models;
 
-import java.util.*;
-import javax.persistence.*;
+import play.db.ebean.Model;
 
-import play.db.ebean.*;
-import play.data.format.*;
-import play.data.validation.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Login extends Model{
 
 	@Id
 	@NotNull
-	@OneToOne(name = "user_id")
 	public User user;
 
 
