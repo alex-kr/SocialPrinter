02ddef70ccad888f9183f0c00cbf21ab0d524f2d
Alex Kryukov <alexx.kryukov@gmail.com>
2015-02-21 23:49:15 +0200

    Add controller mocks

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,6 +5,12 @@ import play.mvc.*;
 
 import views.html.*;
 
+/*
+* Used for home page operations. Login/registration etc.
+* Top menu buttons
+*
+*/
+
 public class Application extends Controller {
 
     public static Result index() {
---
+++ app/controllers/OperationController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+/*
+* Used for print operations
+*
+*/
+
+public class OperationController extends Controller {
+
+
+}
