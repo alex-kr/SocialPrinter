e9c5c7a53a7f44a8bec383ee7baa35ea7642386f
dychko <dichko.v@bigmir.net>
2015-02-21 23:11:31 +0200

    Add printer model

---
+++ app/models/Printer.java
@@ -0,0 +1,32 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+public class Printer extends Model {
+    @Id
+    @NotNull
+    public Long id;
+
+    public String location;
+
+    @Column(name = "pages_available")
+    public int pagesAvailable;
+
+    @ManyToOne
+    public User user;
+
+    @Column(name = "printer_specification")
+    @ManyToOne
+    public PrinterSpecification printerSpecification;
+
+    public static Finder<Long, Printer> find = new Finder<>(Long.class, Printer.class);
+
+    public Printer() {
+
+    }
+}
