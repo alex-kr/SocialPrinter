8965f9b1fc9248171b4fcfc468e7bbb160085231
Alex Kryukov <alexx.kryukov@gmail.com>
2015-02-22 11:30:29 +0200

    profile page

---
+++ public/javascripts/profile.js
@@ -0,0 +1,6 @@
+<script language="JavaScript" type="text/javascript"> 
+$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("active");
+});
+</script>
